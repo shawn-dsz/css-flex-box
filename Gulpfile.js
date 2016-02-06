5bbdfb96ec5f85f10ccabfe19cc5a842c931ed2d
shawn-dsz <shawndsouza@outlook.com>
2016-02-06 16:43:58 +1300

    init

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+var path = {
+  scss: './styles/**/*.scss',
+  jade: './src/**/*.jade',
+  dest: './dest/'
+}
+var gulp = require('gulp')
+var plugins = require('gulp-load-plugins')({
+  rename: {
+    'gulp-jade': 'gulpJade',
+    'jade': 'jade'
+  }
+})
+
+function getTask (task) {
+  return require('./gulp-tasks/' + task)(gulp, plugins, path)
+}
+
+gulp.task('auto-prefix', getTask('autoPrefixer'))
+gulp.task('jade', getTask('jade'))
+gulp.task('sass', getTask('sass'))
+gulp.task('watch', getTask('watch'))
+gulp.task('default', ['watch'])
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# css-flex-box
+# css flex box
+
+## purpose
+
+to learn css flex box
+## usage
+```
+> npm install
+> npm run start
+> npm run dev
+```
+
+Navigate to
+```http://localhost:8080```
+
+
+## reference
+
+[DevTips](https://www.youtube.com/watch?v=G7EIAgfkhmg)
